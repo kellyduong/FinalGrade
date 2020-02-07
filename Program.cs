dd3eebbd8285db4be3838629e3732cadec0a8371
Kelly Duong <kellyduong@usf.edu>
2020-02-06 22:39:26 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinalGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
